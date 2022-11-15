efb123d263131579c0907cf346b552796a082e22
Sofia <sofiabelucci08@gmail.com>
2022-11-14 21:08:26 -0300

    Principio del recuperatorio

---
+++ RecuperatorioBelucciSofia1g/src/RecuperatorioBelucciSofia1g.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : RecuperatorioBelucciSofia1g.c
+ Author      : Sofia Belucci
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+	return 0;
+}
